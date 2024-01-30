86220d361ac7f7d22b183fc2b5fc19f61a90210a
Antonio <rodrigowserra@gmail.com>
2024-01-30 20:40:18 +0000

    Conta POO

---
+++ src/main/java/com/bank/aulas/Conta.java
@@ -0,0 +1,16 @@
+package com.bank.aulas;
+
+public class Conta {
+	private String nif;
+	private String iban;
+	private double saldo;
+	
+	public String getNif() {
+		return this.nif;
+	}
+	
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+}
---
+++ src/main/java/com/bank/aulas/MainPOO.java
@@ -0,0 +1,13 @@
+package com.bank.aulas;
+
+public class MainPOO {
+
+	public static void main(String[] args) {
+		Conta contaUm = new Conta();
+		contaUm.setNif("123456789");
+		
+		System.out.println(contaUm.getNif());
+
+	}
+
+}
