cdb2d2bd1f33b7e0c05c1d3d4a454808d380c6db
Antonio <rodrigowserra@gmail.com>
2024-02-02 19:28:59 +0000

    fix - Transferência

--- src/main/java/com/bank/aulas/Conta.java
+++ src/main/java/com/bank/aulas/Conta.java
@@ -1,36 +1,33 @@
 package com.bank.aulas;
 
-import java.util.Scanner;
-
 public class Conta {
 	private String nif;
 	private String iban;
 	private double saldo;
+	
+	public Conta() {}
+	
+	public Conta(String nif, String iban, double saldo) {
+		this.nif = nif;
+		this.iban = iban;
+		this.saldo = saldo;
+	}
 
 	public void debito(double valor) {
 		this.saldo = this.saldo - valor;
 	}
 
-	public void	credito(double valor) {this.saldo = this.saldo + valor;
+	public void credito(double valor) {
+		this.saldo = this.saldo + valor;
 		this.saldo = (this.saldo * 0.01) + this.saldo;
 	}
 
-	public void transf() {
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Qual é o IBAN da conta que pretende transferir?");
-		String iban = sc.nextLine();
-
-		System.out.println("Qual e o valor que pretende transferir?");
-		double valorTrans = sc.nextDouble();
-
-
-        if (this.saldo < valorTrans){
+	public void transf(double valorTrans, Conta contaCredit) {
+		if (this.saldo < valorTrans) {
 			System.out.println("Nao tem saldo suficiente para realizar a transferencia.");
 		} else {
-			this.saldo = this.saldo - valorTrans;
-
+			this.debito(valorTrans);
+			contaCredit.credito(valorTrans);
 		}
 	}
 
@@ -42,12 +39,20 @@ public class Conta {
 		this.nif = nif;
 	}
 
-	public String getIban() { return this.iban;}
+	public String getIban() {
+		return this.iban;
+	}
 
-	public void setIban(String iban) { this.iban = iban; }
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
 
-	public double getSaldo() { return this.saldo;}
+	public double getSaldo() {
+		return this.saldo;
+	}
 
-	public void setSaldo(double saldo) {this.saldo = saldo;}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 
 }
